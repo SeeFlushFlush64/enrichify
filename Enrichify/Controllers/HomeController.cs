0a1e5ed4499efd103a3e550d10c2bdd401a3f7b0
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-11-21 22:10:16 +0800

    has auth, has hunter api

---
+++ Enrichify/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using CsvHelper;
+using Enrichify.Models;
+using Enrichify.Services; // your HunterService
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Enrichify.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly HunterService _hunterService;
+
+        public HomeController(HunterService hunterService)
+        {
+            _hunterService = hunterService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadCsv(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return View("Index");
+
+            List<Contact> contacts;
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HeaderValidated = null,
+                    MissingFieldFound = null
+                };
+
+                using (var csv = new CsvReader(reader, config))
+                {
+                    contacts = csv.GetRecords<Contact>().ToList();
+                }
+            }
+
+            // Enrich each contact asynchronously
+            foreach (var contact in contacts)
+            {
+                // Split first and last name
+                var names = contact.Name.Split(' ');
+                string firstName = names[0];
+                string lastName = names.Length > 1 ? names[1] : "";
+                contact.EnrichedEmail = await _hunterService.FindEmail(contact.Company, firstName, lastName);
+            }
+
+            // Pass the enriched list directly to the Results view
+            return View("Results", contacts);
+        }
+    }
+}
---
+++ Enrichify/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Enrichify.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string EnrichedEmail { get; set; } // Filled by Hunter.io
+        public string EnrichedDomain { get; set; } // Optional enrichment
+    }
+
+}
